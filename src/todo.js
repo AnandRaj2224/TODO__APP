e9d64c2b1d92d03bc24b140ab52fc54a44e3c22b
Anand <anandraj6783@gmail.com>
2024-09-20 00:56:58 +0530

    added todo and project classes

--- src/todo.js
+++ src/todo.js
@@ -1,23 +1,18 @@
-import { saveToLocalStorage, projects, currentProject, Project, Todo } from './projectManager';
-
-// Function to create a new project object
 export function createNewProjectObj(name) {
-  let project1 = new Project(name);
-  currentProject = project1;
-  projects.push(project1);
-
-  saveToLocalStorage(); // Save the updated projects list
-  return project1;
+  return {
+    name,
+    todo: [],
+    addTodo(todo) {
+      this.todo.push(todo);
+    },
+  };
 }
 
-// Function to create a new todo object
 export function createNewTodoObj(todoName, description, dueDate, priority) {
-  let todo1 = new Todo(todoName, description, dueDate, priority);
-  
-  if (currentProject) {
-    currentProject.addTodo(todo1);
-    saveToLocalStorage(); // Save the updated projects list
-  } else {
-    console.error("No project selected to add a todo to!");
-  }
+  return {
+    todoName,
+    description,
+    dueDate,
+    priority,
+  };
 }
